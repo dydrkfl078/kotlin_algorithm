45f58ba9d86afdde47234fca5f104fc1a3e4f45e
june <dydrkfl078@naver.com>
2024-03-09 05:44:00 +0900

    lv1_array_similarity

---
+++ app/src/main/java/com/example/algorithm_programmers/lv1/Array_Similarity.kt
@@ -0,0 +1,32 @@
+package com.example.algorithm_programmers.lv1
+
+class Array_Similarity {
+    fun solution(s1: Array<String>, s2: Array<String>): Int {
+        var answer: Int = 0
+        for (i in s1 ) {
+            for ( j in s2) {
+                if (i==j) {
+                    answer++
+                }
+            }
+        }
+        return answer
+    }
+}
+
+class Array_Similarity2 {
+    fun solution(s1: Array<String>, s2: Array<String>) : Int {
+        var answer: Int = s1.filter { s2.contains(it)}.size
+
+        return answer
+    }
+}
+
+fun main() {
+    val testFun = Array_Similarity()
+    val testFun2 = Array_Similarity2()
+
+    val testA : (Int) -> Unit = ::println // 메서드 참조 테스트...
+    testA(testFun.solution(arrayOf("테스트","t","e"), arrayOf("t","f")))
+    testA(testFun2.solution(arrayOf("1","2","3"), arrayOf("1","2","3","4","5")))
+}
