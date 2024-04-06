aef410b7912c05f6c2731109113ee37988a9d3ca
june <dydrkfl078@naver.com>
2024-04-06 16:24:12 +0900

    Pass_lv0/array_sort_index

---
+++ app/src/main/java/com/example/algorithm_programmers/lv1/Array_String_Sort.kt
@@ -0,0 +1,26 @@
+package com.example.algorithm_programmers.lv1
+
+private fun solution (strings: Array<String>, n: Int): Array<String> {
+    var answer = mutableListOf<String>()
+
+    var indexChar : MutableList<String> = mutableListOf<String>()
+    strings.toList().chunked(1).map { strings ->
+        var chars  = strings[0].chunked(1)
+        var el = chars[n]
+
+        indexChar.add(el+chars.joinToString(""))
+    }
+    indexChar.sorted().toMutableList().forEach {
+        answer.add(it.removeRange(0 until 1))
+
+    }
+
+    return answer.toTypedArray()
+}
+
+fun main() {
+    var testA = arrayOf("sun","bed","car")
+    var testB = arrayOf("car","pair","fail")
+
+    solution(testA,1)
+}
