c6ccb9d800063c12004564bd6bd72058c30b17ab
june <dydrkfl078@naver.com>
2024-04-06 11:05:48 +0900

    pass_lv0/array_parallel_line

--- app/src/main/java/com/example/algorithm_programmers/lv1/Array_Parallel_Line.kt
+++ app/src/main/java/com/example/algorithm_programmers/lv1/Array_Parallel_Line.kt
@@ -1,33 +1,26 @@
 package com.example.algorithm_programmers.lv1
 
-import kotlin.math.abs
-
 private fun solution(dots: Array<IntArray>): Int {
     var answer = 0
 
-    (dots.indices-(dots.size-1)).map { dotA ->
-        dots.filter { !it.contentEquals(dots[dotA]) }.map { dotB ->
-
+    (dots.indices).map { dotA ->
+        var excluedDotA = dots.filterIndexed { index, _ -> index !=dotA }
+        excluedDotA.mapIndexed { dotBIndex, dotB ->
             var line = listOf(dots[dotA], dotB)
-            var outerLine = dots.filter { !it.contentEquals(line[0]) }.filter { !it.contentEquals(line[1]) }
-            var lengthA_X = arrayOf<IntArray>(intArrayOf(Math.abs(line[0][0]-outerLine[0][0]),Math.abs(line[0][1]-outerLine[0][1])))
-            var lengthA_Y = arrayOf<IntArray>(intArrayOf(Math.abs(line[0][0]-outerLine[1][0]),Math.abs(line[0][1]-outerLine[1][1])))
-            var lengthB_X = arrayOf<IntArray>(intArrayOf(Math.abs(line[1][0]-outerLine[0][0]),Math.abs(line[1][1]-outerLine[0][1])))
-            var lengthB_Y = arrayOf<IntArray>(intArrayOf(Math.abs(line[1][0]-outerLine[1][0]),Math.abs(line[1][1]-outerLine[1][1])))
+            var otherLine = excluedDotA.filterIndexed { index, _ -> index != dotBIndex }
 
-            var compare = listOf( lengthA_X,lengthA_Y,lengthB_X,lengthB_Y)
-            compare.forEachIndexed{ index, intArrays ->
-                (compare.indices-index).map{
-                    println("it = $it , compare[index] = ${compare[index][0].toList()}, compare[it] = ${compare[it][0].toList()}," )
-                    if ( compare[index][0].contentEquals (compare[it][0])) {
-                        answer = 1
-                        println("answer = $answer")
-                    }
-                }
+            println("line = ${line[0].toList()}, ${line[1].toList()} other = ${otherLine[0].toList()}, ${otherLine[1].toList()}")
+            fun inclination(arr : List<IntArray>): Double {
+                var x = arr[1][0].toDouble() - arr[0][0].toDouble()
+                var y = arr[1][1].toDouble() - arr[0][1].toDouble()
+
+                return y / x
             }
-            println(" lengthAX AY BX BY ${lengthA_X[0].toList()} ${lengthA_Y[0].toList()} ${lengthB_X[0].toList()} ${lengthB_Y[0].toList()}")
-            println("${dotA} = ${line[0].toList()} ${line[1].toList()}")
 
+            println("${inclination(line) } ${inclination(otherLine)}")
+            if (inclination(line) == inclination(otherLine)) {
+                answer = 1
+            }
         }
     }
     return answer
@@ -36,6 +29,9 @@ private fun solution(dots: Array<IntArray>): Int {
 
 fun main() {
 
-    var testA = arrayOf(intArrayOf(1, 4), intArrayOf(9, 2), intArrayOf(3, 8), intArrayOf(11, 6))
+    var testA = arrayOf(intArrayOf(10, 1), intArrayOf(9, 3), intArrayOf(2, 2), intArrayOf(4, 4))
+
+    println(testA.filterIndexed { index, ints -> index != 0})
+    var testB = arrayOf(intArrayOf(1, 4), intArrayOf(9, 2), intArrayOf(1, 4), intArrayOf(9, 2))
     solution(testA)
 }
