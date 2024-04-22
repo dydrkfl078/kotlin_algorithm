d8fd0ee1b882f5494e24960bb4ed7631bf19ab23
june <dydrkfl078@naver.com>
2024-04-22 15:31:47 +0900

    Pass_lv1/Map/Failure_late

--- app/src/main/java/programmers_lv1/Calc_Failure_Late.kt
+++ app/src/main/java/programmers_lv1/Calc_Failure_Late.kt
@@ -3,26 +3,22 @@ package programmers_lv1
 private fun solution(N: Int, stages: IntArray): IntArray {
     var answer = intArrayOf()
 
-    val tryStages = (1..N).toList().map { stage ->
+    val tryStages = (1..N+1).toList().map { stage ->
         stages.filter { it == stage }
     }
 
     tryStages.mapIndexed { index, ints ->
-        if (ints.isEmpty()) {
-            listOf(0)
-        } else {
-            tryStages[index]
-        }
-    }.run { this.mapIndexed { index, ints ->
-        Pair((this.drop(index).sumOf { it.count() }.toDouble() / this[index].count { it == index + 1 }),index + 1)
-    }.sortedBy { it.first }.map { it.second }.also { answer += it } }
+        Pair(tryStages.drop(index).sumOf { it.count() } / ints.count().toDouble(), index + 1)
+    }.take(N).sortedBy { it.first }.map { it.second }.also { answer += it }
 
     return answer
 }
 
 fun main() {
     val testA = intArrayOf(2, 1, 2, 6, 2, 3, 3, 4)
+//    val testB = intArrayOf(1,1,2,2)
 
     println(solution(5, testA).toList())
+//    println(solution(3, testB).toList())
 
 }
