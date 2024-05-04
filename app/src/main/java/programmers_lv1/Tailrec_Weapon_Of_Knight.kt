f7be410ff960ec2d6543223d6ec82e1079096f4b
june <dydrkfl078@naver.com>
2024-05-05 06:05:39 +0900

    Fail_lv1/sqrt/Weapon_of_Knight

--- app/src/main/java/programmers_lv1/Tailrec_Weapon_Of_Knight.kt
+++ app/src/main/java/programmers_lv1/Tailrec_Weapon_Of_Knight.kt
@@ -1,31 +1,49 @@
 package programmers_lv1
 
 private fun solution(number: Int, limit: Int, power: Int): Int {
-    var answer: Int = 0
+    var answer = 0
+//    tailrec fun Int.countDividor (n: Int, factor: Int ,count: Int ): Int {
+//        if (n == factor || count > limit) return count + 1
+//        if (n % factor == 0) return this.countDividor(n,factor + 1, count + 1)
+//        return this.countDividor(n, factor + 1, count)
+//    }
 
-    answer = (1..number).toList().map {
-        it.countDividor(it,1,0).run {
-            if (this > limit) {
-                power
-            } else {
-                this
+
+    return (1..number).map {
+        var n = it
+        val primeFactor = mutableListOf<Int>(1)
+
+        while (n % 2 == 0) {
+            primeFactor.add(n)
+            n /= 2
+        }
+
+        (3..Math.sqrt(n.toDouble()).toInt() step 2).forEach { odd ->
+            while (n % odd == 0) {
+                primeFactor.add(n)
+                n /= odd
             }
         }
+
+        if (n > 2) {
+            primeFactor.add(n)
+        }
+
+        println(primeFactor.sorted())
+
+        if (primeFactor.count() > limit) {
+            limit
+        } else {
+            primeFactor.count()
+        }
     }.sum()
-
-    return answer
 }
 
-tailrec fun Int.countDividor (n: Int, factor: Int ,count: Int ): Int {
-    if (n == factor) return count + 1
-    if (n % factor == 0) return this.countDividor(n,factor + 1, count + 1)
-    return this.countDividor(n, factor + 1, count)
-}
 
 fun main() {
     val testN = 10
     val testLimit = 3
     val testPower = 2
 
-    println(solution(testN,testLimit,testPower))
+    println(solution(testN, testLimit, testPower))
 }
