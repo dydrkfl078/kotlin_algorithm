d29494d6edf970586a4db33c5ac2f8939a5c86d6
june <dydrkfl078@naver.com>
2024-06-06 07:10:07 +0900

    :recycle:for문에서 forEachIndexed 문으로 수정

--- app/src/main/java/programmers_lv2/H_Index_정렬.kt
+++ app/src/main/java/programmers_lv2/H_Index_정렬.kt
@@ -1,20 +1,23 @@
 package programmers_lv2
 
 private fun solution(citations: IntArray): Int {
-    var answer = 0
-
-    val a = citations.sortedDescending()
-
-    // forEachIndexed 문으로 돌리고 싶은데, forEach는 원하는 값을 찾았을 때 중단이 불가능.
-    for (i in a.indices) {
-        if (a[i] > i) {
-            answer++
-        } else {
-            break
-        }
+    citations.sortedDescending().forEachIndexed { index, i ->
+        println(index)
+        if (i < index) return index
     }
+    return citations.size
 
-    return answer
+//    val a = citations.sortedDescending()
+    // forEachIndexed 문으로 돌리고 싶은데, forEach는 원하는 값을 찾았을 때 중단이 불가능.
+//    for (i in a.indices) {
+//        if (a[i] > i) {
+//            answer++
+//        } else {
+//            break
+//        }
+//    }
+
+//    return answer
 }
 
 fun main() {
